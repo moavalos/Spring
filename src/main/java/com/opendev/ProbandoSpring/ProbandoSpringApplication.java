9afd3371d3a30350b25f7ee458bb0efe4cf46de9
avalos.m <mora_m_avalos@hotmail.com>
2023-03-29 13:22:37 -0300

    first

---
+++ src/main/java/com/opendev/ProbandoSpring/ProbandoSpringApplication.java
@@ -0,0 +1,13 @@
+package com.opendev.ProbandoSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProbandoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProbandoSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/opendev/ProbandoSpring/ProbandoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.opendev.ProbandoSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProbandoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
